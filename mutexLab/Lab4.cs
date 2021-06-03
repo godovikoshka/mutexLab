181ce513f7f2665268a5cdf70b262011ead47e10
godovikoshka <godovikoshka@gmail.com>
2021-06-03 17:07:13 +0300

    Добавьте файлы проекта.

---
+++ mutexLab/Lab4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace mutexLab
+{
+    class Lab4
+    {
+        static void Main(string[] args)
+        {
+            bool createdNew;
+
+            Mutex m = new Mutex(true, "myApp", out createdNew);
+
+            if (!createdNew)
+            {
+                // myApp is already running...
+                Console.WriteLine("myApp is already running!", "Multiple Instances");
+                return;
+            }
+        }
+    }
+}
